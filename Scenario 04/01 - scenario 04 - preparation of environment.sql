5947e133d41761af79843d42c6ab9b7bfe8c326d
uricken1964 <87075518+uricken1964@users.noreply.github.com>
2025-01-02 16:01:50 +0100

    2025-01-02

--- Scenario 04/01 - scenario 04 - preparation of environment.sql
+++ Scenario 04/01 - scenario 04 - preparation of environment.sql
@@ -2,7 +2,12 @@
 	============================================================================
 	File:		0081 - scenario 04 - preparation of environment.sql
 
-	Summary:	This script creates the environment for the scenario.
+	Summary:	A customer's webshop runs without any complaints from customers during "normal" days.
+				As soon as special sales promotions are started (e.g. Black Friday, fire sale, ...),
+				the system's performance collapses and customers complain that it takes
+				a long time for an order to be saved in the system.
+	
+				This script creates the environment for the scenario.
 				- new table webshop.customers
 				- new table webshop.orders
 				- new function for the calculation of num of orders
@@ -35,11 +40,15 @@
 USE ERP_Demo;
 GO
 
+SET NOCOUNT ON;
+GO
+
 EXEC dbo.sp_create_indexes_customers;
 EXEC dbo.sp_create_indexes_orders;
 GO
 
 /* let's create the required tables first */
+DROP TABLE IF EXISTS webshop.lineitems;
 DROP TABLE IF EXISTS webshop.orders;
 DROP TABLE IF EXISTS webshop.customers;
 GO
@@ -50,19 +59,38 @@ IF SCHEMA_ID(N'webshop') IS NULL
 GO
 
 /* create the table webshop.customers with 1.6 Mio customers */
-SELECT	*,
+RAISERROR ('Creating table webshop.customers...', 0, 1) WITH NOWAIT;
+SELECT	[c_custkey],
+		[c_mktsegment],
+		[c_nationkey],
+		[c_name],
+		[c_address],
+		[c_phone],
+		[c_acctbal],
+		[c_comment],
 		CAST(0 AS INT)	AS [num_orders]
 INTO	webshop.customers
 FROM	dbo.customers;
 GO
 
 ALTER TABLE webshop.customers
-ADD CONSTRAINT pk_webshop_customers PRIMARY KEY CLUSTERED (c_custkey)
-WITH (DATA_COMPRESSION = PAGE);
+ADD CONSTRAINT pk_webshop_customers
+PRIMARY KEY CLUSTERED (c_custkey)
+WITH (SORT_IN_TEMPDB = ON, DATA_COMPRESSION = PAGE);
 GO
 
 /* create the empty table webshop.orders with no records */
-SELECT	*
+RAISERROR ('Creating table webshop.orders...', 0, 1) WITH NOWAIT;
+SELECT	[o_orderdate],
+		[o_orderkey],
+		[o_custkey],
+		[o_orderpriority],
+		[o_shippriority],
+		[o_clerk],
+		[o_orderstatus],
+		[o_totalprice],
+		[o_comment],
+		[o_storekey]
 INTO	webshop.orders
 FROM	dbo.orders
 WHERE	1 = 0;
@@ -70,17 +98,17 @@ GO
 
 ALTER TABLE webshop.orders
 ADD CONSTRAINT pk_webshop_orders PRIMARY KEY CLUSTERED (o_orderkey)
-WITH (DATA_COMPRESSION = PAGE);
+WITH (SORT_IN_TEMPDB = ON, DATA_COMPRESSION = PAGE);
 GO
 
 CREATE NONCLUSTERED INDEX nix_webshop_orders_o_orderdate
 ON webshop.orders (o_orderdate)
-WITH (DATA_COMPRESSION = PAGE);
+WITH (SORT_IN_TEMPDB = ON, DATA_COMPRESSION = PAGE);
 GO
 
 CREATE NONCLUSTERED INDEX nix_webwhop_orders_o_custkey
 ON webshop.orders (o_custkey)
-WITH (DATA_COMPRESSION = PAGE);
+WITH (SORT_IN_TEMPDB = ON, DATA_COMPRESSION = PAGE);
 GO
 
 ALTER TABLE webshop.orders
@@ -88,7 +116,30 @@ ADD CONSTRAINT fk_webshop_customers FOREIGN KEY (o_custkey)
 REFERENCES webshop.customers (c_custkey);
 GO
 
+RAISERROR ('Creating table webshop.lineitems...', 0, 1) WITH NOWAIT;
+SELECT	*
+INTO	webshop.lineitems
+FROM	dbo.lineitems
+WHERE	1 = 0;
+GO
+
+ALTER TABLE webshop.lineitems
+ADD CONSTRAINT pk_webshop_lineitems PRIMARY KEY CLUSTERED
+(
+	l_orderkey,
+	l_linenumber
+)
+WITH (SORT_IN_TEMPDB = ON, DATA_COMPRESSION = PAGE);
+GO
+
+ALTER TABLE webshop.lineitems
+ADD CONSTRAINT fk_webshop_orders FOREIGN KEY (l_orderkey)
+REFERENCES webshop.orders (o_orderkey);
+GO
+
 /* A function is needed for the evaluation of the total num of orders for a customer */
+RAISERROR ('Creating function webshop.total_orders...', 0, 1) WITH NOWAIT;
+GO
 CREATE OR ALTER FUNCTION webshop.total_orders(@o_custkey BIGINT, @o_orderyear INT)
 RETURNS INT
 AS
@@ -117,6 +168,9 @@ GO
 		both customers +n/-n
 	- if the order will be deleted we must update the webshop, customers table, too
 */
+RAISERROR ('Creating triggers on webshop.orders...', 0, 1) WITH NOWAIT;
+GO
+
 CREATE OR ALTER TRIGGER webshop.trg_orders_insert
 ON webshop.orders
 FOR INSERT
@@ -173,8 +227,10 @@ BEGIN
 	FOR
 		SELECT	i.o_custkey				AS	new_o_custkey,
 				YEAR(i.o_orderdate)		AS	new_o_orderyear,
+				i.o_orderstatus			AS	new_o_orderstatus,
 				d.o_custkey				AS	old_o_custkey,
-				YEAR(d.o_orderdate)		AS	old_o_orderyear
+				YEAR(d.o_orderdate)		AS	old_o_orderyear,
+				d.o_orderstatus			AS	old_o_orderstatus
 		FROM	inserted AS i
 				INNER JOIN deleted AS d
 				ON (i.o_orderkey = d.o_orderkey);
@@ -185,17 +241,20 @@ BEGIN
 
 	WHILE @@FETCH_STATUS = 0
 	BEGIN
-		SET	@actual_num_of_orders = webshop.total_orders(@new_o_custkey, @new_o_orderyear);
+		IF @new_o_custkey <> @old_o_custkey
+		BEGIN
+			SET	@actual_num_of_orders = webshop.total_orders(@new_o_custkey, @new_o_orderyear);
 		
-		UPDATE	webshop.customers
-		SET		num_orders = @actual_num_of_orders
-		WHERE	c_custkey = @new_o_custkey;
+			UPDATE	webshop.customers
+			SET		num_orders = @actual_num_of_orders
+			WHERE	c_custkey = @new_o_custkey;
 
-		SET	@actual_num_of_orders = webshop.total_orders(@old_o_custkey, @old_o_orderyear);
+			SET	@actual_num_of_orders = webshop.total_orders(@old_o_custkey, @old_o_orderyear);
 		
-		UPDATE	webshop.customers
-		SET		num_orders = @actual_num_of_orders
-		WHERE	c_custkey = @old_o_custkey;
+			UPDATE	webshop.customers
+			SET		num_orders = @actual_num_of_orders
+			WHERE	c_custkey = @old_o_custkey;
+		END
 
 		FETCH NEXT FROM c INTO @new_o_custkey, @new_o_orderyear, @old_o_custkey, @old_o_orderyear;
 	END
@@ -248,48 +307,70 @@ GO
 	Let's create stored procedures for a better handling / demonstration of the process
 	each stord procedure will cover one event (INSERT, UPDATE, DELETE)
 */
+RAISERROR ('Creating stored procedures for webshop.orders...', 0, 1) WITH NOWAIT;
+GO
+
 CREATE OR ALTER PROCEDURE webshop.insert_order_record
 	@c_custkey		BIGINT,
-	@num_records	INT
+	@num_records	INT	=	1
 AS
 BEGIN
 	SET NOCOUNT ON;
 	SET XACT_ABORT ON;
 
-	;WITH d
-	AS
-	(
-		SELECT	CAST(GETDATE() AS DATE)	AS	o_orderdate,
-				o_orderkey,
-				o_custkey,
-				o_orderpriority,
-				o_shippriority,
-				o_clerk,
-				o_orderstatus,
-				o_totalprice,
-				o_comment
-		FROM	dbo.orders
-		WHERE	o_custkey = @c_custkey
+	BEGIN TRANSACTION
+		;WITH d
+		AS
+		(
+			SELECT	CAST(GETDATE() AS DATE)	AS	o_orderdate,
+					o_orderkey,
+					o_custkey,
+					o_orderpriority,
+					o_shippriority,
+					o_clerk,
+					o_totalprice,
+					o_comment
+			FROM	dbo.orders
+			WHERE	o_custkey = @c_custkey
 
-		EXCEPT
+			EXCEPT
 
-		SELECT	CAST(GETDATE() AS DATE)	AS	o_orderdate,
-				o_orderkey,
-				o_custkey,
-				o_orderpriority,
-				o_shippriority,
-				o_clerk,
-				o_orderstatus,
-				o_totalprice,
-				o_comment
-		FROM	webshop.orders
-		WHERE	o_custkey = @c_custkey
-	)
-	INSERT INTO webshop.orders
-	(o_orderdate, o_orderkey, o_custkey, o_orderpriority, o_shippriority, o_clerk, o_orderstatus, o_totalprice, o_comment)
-	SELECT	TOP (@num_records)
-	d.o_orderdate, d.o_orderkey, d.o_custkey, d.o_orderpriority, d.o_shippriority, d.o_clerk, d.o_orderstatus, d.o_totalprice, d.o_comment
-	FROM d;
+			SELECT	CAST(GETDATE() AS DATE)	AS	o_orderdate,
+					o_orderkey,
+					o_custkey,
+					o_orderpriority,
+					o_shippriority,
+					o_clerk,
+					o_totalprice,
+					o_comment
+			FROM	webshop.orders
+			WHERE	o_custkey = @c_custkey
+		)
+		INSERT INTO webshop.orders
+		(
+			o_orderdate,
+			o_orderkey,
+			o_custkey,
+			o_orderpriority,
+			o_shippriority,
+			o_clerk,
+			o_orderstatus,
+			o_totalprice,
+			o_comment
+		)
+		SELECT	TOP (@num_records)
+				d.o_orderdate,
+				d.o_orderkey,
+				d.o_custkey,
+				d.o_orderpriority,
+				d.o_shippriority,
+				d.o_clerk,
+				'N'				AS	o_orderstatus,
+				d.o_totalprice,
+				d.o_comment
+		FROM d;
+
+		/* We insert a few lineitems to the order */
 
 	SELECT	c_custkey,
             c_mktsegment,
@@ -311,7 +392,7 @@ GO
 CREATE OR ALTER PROCEDURE webshop.move_order_record
 	@from_custkey	BIGINT,
 	@to_custkey		BIGINT,
-	@num_records	INT
+	@num_records	INT	=	1
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -357,7 +438,7 @@ GO
 
 CREATE OR ALTER PROCEDURE webshop.delete_order_record
 	@c_custkey		BIGINT,
-	@num_records	INT
+	@num_records	INT	= 1
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -384,13 +465,37 @@ BEGIN
 END
 GO
 
+CREATE OR ALTER PROCEDURE webshop.update_order_record
+	@delay VARCHAR(10) = '00:00:05'
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+
+	DECLARE	@o_orderkey BIGINT = (SELECT TOP (1) o_orderkey FROM webshop.orders WHERE o_orderstatus = 'N');
+
+	BEGIN TRANSACTION
+		UPDATE	webshop.orders
+		SET		o_orderstatus = 'P'
+		WHERE	o_orderkey = @o_orderkey;
+
+		/* The delay simulates a process done by the application */
+		WAITFOR DELAY @delay;
+	COMMIT TRANSACTION
+END
+GO
+
 
 /*
 	These stored procedures are for the stress test scenarios only
 	The functionality is primitive because it grabs a random customer_id
 	and insert/update/delete an order.
 */
+RAISERROR ('Creating wrapper procedures for webshop.orders...', 0, 1) WITH NOWAIT;
+GO
+
 CREATE OR ALTER PROCEDURE webshop.insert_order
+	@num_records	INT = 1
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -407,11 +512,11 @@ BEGIN
 	/* Insert an order for this customer */
 	EXEC webshop.insert_order_record
 		@c_custkey = @c_custkey,
-	    @num_records = 1;
+	    @num_records = @num_records;
 END
 GO
 
-CREATE OR ALTER PROCEDURE webshop.update_order
+CREATE OR ALTER PROCEDURE webshop.move_order
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -440,6 +545,15 @@ BEGIN
 END
 GO
 
+CREATE OR ALTER PROCEDURE webshop.update_order
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+
+END
+GO
+
 CREATE OR ALTER PROCEDURE webshop.delete_order
 AS
 BEGIN
@@ -459,44 +573,6 @@ BEGIN
 		@c_custkey = @c_custkey,
 	    @num_records = 1;	
 END
-GO
-
-/*
-	Last step is the implementation of an XEvent to see the call stack
-*/
-IF EXISTS (SELECT * FROM sys.dm_xe_sessions WHERE name = N'scenario 04 - tracking single process')
-	DROP EVENT SESSION [scenario 04 - tracking single process] ON SERVER;
-	GO
-
-CREATE EVENT SESSION [scenario 04 - tracking single process]
-ON SERVER 
-	ADD EVENT sqlserver.sp_statement_completed
-	(
-		WHERE	sqlserver.is_system = 0
-				AND sqlserver.database_name = N'ERP_Demo'
-	),
-	ADD EVENT sqlserver.sql_statement_completed
-	(
-		WHERE	sqlserver.is_system = 0
-				AND sqlserver.database_name = N'ERP_Demo'
-	),
-	ADD EVENT sqlserver.database_xml_deadlock_report
-	(
-		WHERE database_name=N'ERP_Demo'
-	)
-WITH
-(
-	MAX_MEMORY=4096 KB,
-	EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,
-	MAX_DISPATCH_LATENCY=30 SECONDS,
-	MAX_EVENT_SIZE=0 KB,
-	MEMORY_PARTITION_MODE=NONE,
-	TRACK_CAUSALITY=ON,
-	STARTUP_STATE=OFF
-);
-GO
-
-ALTER EVENT SESSION [scenario 04 - tracking single process] ON SERVER STATE = START;
 GO
 
 ALTER DATABASE ERP_Demo SET QUERY_STORE CLEAR;
