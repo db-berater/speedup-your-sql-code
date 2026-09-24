dc9637a5325ebde456428cb5063476f935018ffc
db-berater <87075518+uricken1964@users.noreply.github.com>
2026-09-24 07:29:46 +0200

    SQL Days 2026

--- 04 - Workshop Scenarios/Scenario 05/02 - scenario 05 - optimization 01.sql
+++ 04 - Workshop Scenarios/Scenario 05/02 - scenario 05 - optimization 01.sql
@@ -17,16 +17,6 @@
 	Revion:		February 2025
 
 	SQL Server Version: >= 2016
-	------------------------------------------------------------------------------
-	Written by Uwe Ricken, db Berater GmbH
-
-	This script is intended only as a supplement to demos and lectures
-	given by Uwe Ricken.  
-  
-	THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF 
-	ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED 
-	TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
-	PARTICULAR PURPOSE.
 	============================================================================
 */
 SET NOCOUNT ON;
